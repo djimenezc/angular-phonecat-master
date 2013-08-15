89c59e3533f143d0b45ec3d6425ba2aec58e1938
David Jimenez <d.jimenez@sap.com>
2013-08-15 13:39:04 +0100

    Added router module
    Signed-off-by: David Jimenez <d.jimenez@sap.com>

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,20 +1,42 @@
-var http = require('http');
+//var http = require('http');
+//
+//http.createServer(function(req, res) {
+//	res.writeHead(200, {
+//		'Content-Type' : 'text/plain'
+//	});
+//	res.end('Hello World\n');
+//}).listen(1337, '127.0.0.1');
+//
+//console.log('Server running at http://127.0.0.1:1337/');
+//
+//function say(word) {
+//	console.log(word);
+//}
+//
+//function execute(someFunction, value) {
+//	someFunction(value);
+//}
+//
+//execute(say, "Hello");
 
-http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type' : 'text/plain'
-	});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/');
+var http = require("http");
+var url = require("url");
 
-function say(word) {
-	console.log(word);
+function start(route) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(pathname);
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started at http://127.0.0.1:8888.");
 }
 
-function execute(someFunction, value) {
-	someFunction(value);
-}
-
-execute(say, "Hello");
+exports.start = start;
