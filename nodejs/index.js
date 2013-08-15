97038d2cc3bccf55f08039ccfdfdfb7dfbac368b
David Jimenez <d.jimenez@sap.com>
2013-08-15 17:07:34 +0100

    Implement operation with not blocked calls
    Signed-off-by: David Jimenez <d.jimenez@sap.com>

--- nodejs/index.js
+++ nodejs/index.js
@@ -7,6 +7,7 @@ var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/find"] = requestHandlers.find;
 
 server.start(router.route, handle);
 
--- nodejs/server.js
+++ nodejs/server.js
@@ -1,43 +1,12 @@
-//var http = require('http');
-//
-//http.createServer(function(req, res) {
-//	res.writeHead(200, {
-//		'Content-Type' : 'text/plain'
-//	});
-//	res.end('Hello World\n');
-//}).listen(1337, '127.0.0.1');
-//
-//console.log('Server running at http://127.0.0.1:1337/');
-//
-//function say(word) {
-//	console.log(word);
-//}
-//
-//function execute(someFunction, value) {
-//	someFunction(value);
-//}
-//
-//execute(say, "Hello");
-
-
 var http = require("http");
 var url = require("url");
-
-function start(route,handle) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
-
-    route(handle, pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var content = route(handle, pathname);
-    response.write(content);
-    response.end();
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started at http://127.0.0.1:8888.");
+function start(route, handle) {
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		route(handle, pathname, response);
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
 }
-
 exports.start = start;
